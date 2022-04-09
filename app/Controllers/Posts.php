38bd884b996b5797146dc6c2be94c81db4e08545
Thomas Seng Hin Mak <mak@makzan.net>
2022-04-09 09:37:22 +0800

    Add CRUD action functions to Controller.

--- app/Controllers/Posts.php
+++ app/Controllers/Posts.php
@@ -8,6 +8,51 @@ class Posts extends BaseController
 {
     public function index()
     {
-        //
+        $db = db_connect();
+        $data["posts"] = $this->Posts->findAll();
+        // $data["posts"] = $db->query("SELECT * FROM posts");
+        echo view("posts/index", $data);
     }
+
+    public function show($id)
+    {
+        $post = $this->Posts->find($id);
+        var_dump($post);
+    }
+
+    public function create()
+    {
+        echo view("posts/create");
+    }
+
+    public function save()
+    {
+        $data = $this->request->getPost();
+        $this->Posts->insert($data);
+
+        return redirect()->to('/posts/index');
+    }
+
+    public function edit($id)
+    {
+        $data["post"] = $this->Posts->find($id);
+        echo view("posts/edit", $data);
+    }
+
+    public function update($id)
+    {
+        $data = $this->request->getPost();
+        $this->Posts->update($id, $data);
+
+        return redirect()->to('/posts/');
+    }
+
+    public function delete($id)
+    {
+        $this->Posts->delete($id);
+        return redirect()->to('/posts/');
+    }
+
+
+
 }
