03dbdf60ec3a4aa9342917f0e69d3d3b039d159b
Thomas Seng Hin Mak <mak@makzan.net>
2022-04-09 09:37:08 +0800

    Configure Posts model with allowed fields, use soft delete, use timestamp.

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -12,12 +12,12 @@ class Posts extends Model
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
+    protected $useSoftDeletes   = true;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['title', 'content'];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
